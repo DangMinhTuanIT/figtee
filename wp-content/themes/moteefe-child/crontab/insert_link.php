3df6ebd19603118a618dc125b436749bb02e4bae
thietkeweb5s.net <mtuan2356@gmail.com>
2020-09-17 10:10:05 +0700

    insert product

---
+++ wp-content/themes/moteefe-child/crontab/insert_link.php
@@ -0,0 +1,21 @@
+<?php 
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://figtee.com/insert-link/?tuandm=tuan74123",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "Cookie: PHPSESSID=k7tkrm53ohkpdldigdt63khhc1"
+  ),
+));
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$response = curl_exec($curl);
+
+curl_close($curl);
